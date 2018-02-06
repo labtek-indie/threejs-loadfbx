7d6e2d340857ba758ba8ef435516ac68ba552069
elianalien <elian.alien19@gmail.com>
2018-02-06 14:06:06 +0700

    minion texture fixed on fbx (blender)

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ var container, mesh;
 
 var wolf = 'model/Wolf-Rigged-and-Game-Ready/Wolf_fbx.fbx';
 var capoeira = 'model/Capoeira.fbx';
-var minion = 'Minion_FBX.fbx';
+var minion = 'Minion_FBX1.fbx';
+var box = 'assets/aaa.fbx'
 
 init();
 render();
@@ -14,8 +15,10 @@ function init(){
     document.body.appendChild(container)
 
     // camera
-    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 2000 );
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000 );
     camera.position.set(0, -90, 0);
+    camera.position.z = 1000;
+    // camera.position.y = -90;    
 
     // scene
     scene = new THREE.Scene();
@@ -34,7 +37,7 @@ function init(){
     scene.add( plane );
 
     // ambient light
-    var ambient = new THREE.AmbientLight( 0x101030, 4.0);
+    var ambient = new THREE.AmbientLight( 0x101030, 1.0);
     scene.add(ambient);
 
     light = new THREE.HemisphereLight(0xffffff, 0x444444, 1.0);
@@ -43,7 +46,7 @@ function init(){
 
     // spotlight
     var spotlight = new THREE.SpotLight( 0xffffff );
-    spotlight.position.set(1000,-1000,1000);
+    spotlight.position.set(10,-10,10);
     spotlight.castShadow = true;
     spotlight.shadow.mapSize.width = 1024;
     spotlight.shadow.mapSize.height = 1024;
@@ -61,29 +64,30 @@ function init(){
         // wolf,
         // capoeira,
         minion,
+        // box,
         function(object){
             mesh = object;
-            mesh.scale.set(2, 2, 2);
+            mesh.scale.set(20, 20, 20);
             // mesh.scale.set(1, 1, 1);
-            mesh.rotation.x = 90;
+            mesh.rotation.x = 0;
             scene.add(mesh);
         }
     )
 
     // texture loader
-    var textureLoader = new THREE.TextureLoader();
-    textureLoader.setCrossOrigin("anonymous");
-    textureLoader.load('Textures/jeans_texture.jpg', function (texture) {
-        // mesh is a group contains multiple sub-objects. Traverse and apply texture to all. 
-        mesh.traverse(function (child) {
-            if (child instanceof THREE.Mesh) {
+    // var textureLoader = new THREE.TextureLoader();
+    // textureLoader.setCrossOrigin("anonymous");
+    // textureLoader.load('Textures/jeans_texture.jpg', function (texture) {
+    //     // mesh is a group contains multiple sub-objects. Traverse and apply texture to all. 
+    //     mesh.traverse(function (child) {
+    //         if (child instanceof THREE.Mesh) {
 
-            // apply texture
-            child.material.map = texture
-            child.material.needsUpdate = true;
-            }
-        });
-    });
+    //         // apply texture
+    //         child.material.map = texture
+    //         child.material.needsUpdate = true;
+    //         }
+    //     });
+    // });
 }
 
 function render(){
